766c5fd8cf6f9a7cdab36fc9035db62a233245fc
Kenneth Altes <kennethaltes27@gmail.com>
2024-11-05 13:14:36 +0800

    test33

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
-import path from "path";
 
 import authRoutes from "./router/auth.router.js";
 import jobOrderRoutes from "./router/joborder.route.js";
@@ -13,11 +12,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-const __dirname = path.resolve();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true 
 };
 
@@ -25,17 +22,12 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.use("/api/job-orders", jobOrderRoutes);
 app.use("/api/auth", authRoutes);
-
-// Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'capstone-dashboard-v2/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'capstone-dashboard-v2', 'dist', 'index.html'));
-  });
-}
 
 app.listen(PORT, () => {
     connectDB();
